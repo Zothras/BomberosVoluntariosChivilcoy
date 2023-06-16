4779f47909ae00d489e84c8ba13199f21ae44ffd
Strider <federcioisla1@gmail.com>
2023-06-16 13:21:00 -0300

    Comienzo de salida de materiales peligrosos realizado: solo la vista de de las cargas de datos e implementacion de un viewmodel de damnificados  falta temrinar: algunos datos  y cargarlo en la base dde datos

---
+++ Vista/Data/ViewModels/Personal/DamnificadoViewModels.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vista.Data.Enums;
+
+namespace Vista.Data.ViewModels.Personal
+{
+    public class DamnificadoViewModels
+    {
+        [Required, StringLength(255)]
+        public string Nombre { get; set; }
+        [Required, StringLength(255)]
+        public string Apellido { get; set; }
+        [Required, StringLength(255)]
+        public string Dni { get; set; }
+        public TipoSexo Sexo { get; set; }
+        [Required, StringLength(255)]
+        public string LugarDeNacimiento { get; set; }
+        public int Edad { get; set; }
+        public TipoDamnificado Estado { get; set; }
+
+        [Required, StringLength(255)]
+        public string Marca { get; set; }
+        [Required, StringLength(255)]
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        [Required, StringLength(255)]
+        public string Patente { get; set; }
+        [Required, StringLength(255)]
+        public string Tipo { get; set; }
+
+        [Required, StringLength(255)]
+        public string Color { get; set; }
+        public bool? Airbag { get; set; }
+
+    }
+}
