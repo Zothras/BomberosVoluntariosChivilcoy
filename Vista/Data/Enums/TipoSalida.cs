d6a35cefbca3c1265ad4e302d3ae7649eed89091
BrunGym <brunogimenez2107@gmail.com>
2023-07-04 12:02:40 -0300

    Estoy editando tabla de salidas en pagina Legajo. Estoy trayendo todos los objetos anidados a salidas, para poder mostrarlos o editarlos. Tambien cree un enum de Tipo Salida el cual lo uso igualando el valor traido del discriminador con estos

---
+++ Vista/Data/Enums/TipoSalida.cs
@@ -0,0 +1,23 @@
+﻿namespace Vista.Data.Enums
+{
+    public enum TipoSalida
+    {
+        Accidente,
+        FactorClimatico,
+        MaterialPeligroso,
+        ServicioEspecialPrevencionViewModels,
+        RescateAnimal,
+        RescatePersona,
+        IncendioComercio,
+        IncendioEstablecimientoEducativo,
+        IncendioEstablecimientoPublico,
+        IncendioForestal,
+        IncendioHospitalesYClinicas,
+        IncendioIndustria,
+        IncendioVivienda,
+        ServicioEspecialPrevencion,
+        Incendio,
+        ServicioEspecial
+
+    }
+}
