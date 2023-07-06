72d8ba0c8a33ff92be8a7492ad6fe5d316dcc18b
Federico Jose Isla <federicoisla1@gmail.com>
2023-07-06 11:37:50 -0300

    agregre una nueva migracion para los datos de moviles salidas y creee el modal

--- Vista/Data/ViewModels/Personal/MovilSalidaViewModels.cs
+++ Vista/Data/ViewModels/Personal/MovilSalidaViewModels.cs
@@ -6,12 +6,11 @@ namespace Vista.Data.ViewModels.Personal
     {
         public bool CargoCombustible { get; set; }
         public string? NumeroFactura { get; set; }
-        public string? FechaFactura { get; set; }
+        public DateTime? FechaFactura { get; set; }
         public string? TipoConbustible { get; set; }
         public string? CantidadLitros { get; set; }
-        public string? Usuario { get; set; }
-        public string? NombreYApellidoRecpetor { get; set; }
-        public string? TelefonoReceptor { get; set; }
+        public string? QuienLleno { get; set; }
+        public string? TelefonoQuienLleno { get; set; }
         public Bombero Chofer { get; set; }
         public int NumeroLegajo { get; set; }
         public Movil MovilesSalida { get; set; }
