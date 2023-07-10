3a283cd2b796ed215e9034f56f3b9f74ba12ef6a
joaco <joaco@DESKTOP-KCPCDBH>
2023-07-10 17:54:16 -0300

    Vista del index Arreglos en otras vistas como agregar/editar movil y la sidebar.

--- Vista/wwwroot/pathFoto.js
+++
@@ -1,5 +0,0 @@
-let fotoMovil = document.querySelector('#fotoMovil');
-fotoMovil.addEventListener('change',() => {
-    document.querySelector('#path').innerText =
-        fotoMovil.files[0].name;
-});
