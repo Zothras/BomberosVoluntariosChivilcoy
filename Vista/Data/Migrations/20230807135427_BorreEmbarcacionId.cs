d7f172df0ff434a0e551ebba6361fc49e7e43a1b
Federico Isla <federcioisla1@gmail.com>
2023-08-07 11:39:28 -0300

    Cree Migracion para sacar el aatributo de clave foranea de embarcacion ya que no era necesario

---
+++ Vista/Data/Migrations/20230807135427_BorreEmbarcacionId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    public partial class BorreEmbarcacionId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmbarcacionId",
+                table: "Vehiculo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmbarcacionId",
+                table: "Vehiculo",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
