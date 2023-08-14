d84d820cc95c5343add92323f9be4fa4ca062cb0
BrunGym <brunogimenez2107@gmail.com>
2023-08-14 09:21:47 -0300

    Cree nuevo viewModel para mostrar salidas y editarlas Cree componenetes para separar codigo de legajos
    Editar arreglar
    En todos falta hacer try catch y sacar BBDD de cada llamada
    Ademas me falta llamar a Editar salida desde Legajos

---
+++ Vista/Data/ViewModels/SalidasViewModel1.cs
@@ -0,0 +1,30 @@
+﻿using Vista.Data.Enums;
+using Vista.Data.Models.Personales;
+using Vista.Data.Models.Salidas.Componentes;
+
+namespace Vista.Data.ViewModels
+{
+    public class SalidasViewModel1 : SalidasViewModels
+    {
+        public int SalidaId { get; set; }
+
+
+        public string? Receptor { get; set; }
+
+        public int? ReceptorId { get; set; }
+
+
+        public Bombero? ReceptorBombero { get; set; }
+
+        public int? SeguroId { get; set; }
+        public SeguroSalida? Seguro { get; set; }
+
+
+        public int EncargadoId { get; set; }
+        public Bombero Encargado { get; set; }
+
+        public int QuienLlenoId { get; set; }
+        public Bombero QuienLleno { get; set; }
+        public TipoSalida Discriminador { get; set; }
+    }
+}
