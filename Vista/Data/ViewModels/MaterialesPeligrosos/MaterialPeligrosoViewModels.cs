a7706d6a278261fbd82460d2bbb4cbae67dcdadd
Federico Jose Isla <federicoisla1@gmail.com>
2023-08-15 09:58:39 -0300

    Accidentes tiene las tablas, dispose y demas

--- Vista/Data/ViewModels/MaterialesPeligrosos/MaterialPeligrosoViewModels.cs
+++ Vista/Data/ViewModels/MaterialesPeligrosos/MaterialPeligrosoViewModels.cs
@@ -9,8 +9,7 @@ namespace Vista.Data.ViewModels.MaterialesPeligrosos
         public TipoMaterialPeligroso Tipo { get; set; }
         [Required, StringLength(255)]
         public string Sustancias { get; set; }
-        public int KmLlegada { get; set; }  
-        public int Kilometraje { get; set; }
+        
         //Acciones sobre los materiales
         public bool Controlada { get; set; }
         public bool Venteo { get; set; }
