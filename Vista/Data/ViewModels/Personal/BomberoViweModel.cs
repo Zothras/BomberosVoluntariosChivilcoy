d978e484b284f4efc8eb477386facd180125a657
joaco <joaco@DESKTOP-KCPCDBH>
2023-08-24 17:01:53 -0300

    ...

--- Vista/Data/ViewModels/Personal/BomberoViweModel.cs
+++ Vista/Data/ViewModels/Personal/BomberoViweModel.cs
@@ -69,6 +69,7 @@ namespace Vista.Data.ViewModels.Personal
         public string NombreImagen { get; set; }
         //Brigada-------------------------------
         public string NombreBrigada { get; set; }
-       
+
+        public bool Selected { get; set; }
     }
 }
