f940131948203d81b60447b47a16c53f81b4f168
CUMaty <matuzarabozo@gmail.com>
2023-10-02 10:08:12 -0300

    Empeze con la parte depositos

---
+++ Vista/Data/Models/Salidas/Componentes/Deposito.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration.Internal;
+
+namespace Vista.Data.Models.Salidas.Componentes
+{
+    public class Deposito
+    {
+        public int DepositoId { get; set; }
+        [StringLength(255)]
+        public string Materiales { get; set; }
+        
+        public DateTime SalidaMaterial {  get; set; }
+        
+        public DateTime EntradaMaterial { get; set; }
+    }
+}
