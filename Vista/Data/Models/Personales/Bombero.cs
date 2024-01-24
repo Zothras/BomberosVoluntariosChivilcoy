b4250ebab73c28b3c713a243636d1bec5128010d
Federico Jose Isla <federicoisla1@gmail.com>
2024-01-24 14:45:51 -0300

    hice algunos cambios en el modelo, agregue que en el movil haya una lista de bomberos los cuales van hacer los vinculados para poder limpiar el movil y a los bomberos  le agregue el movil a cargo de limpieza, asu vez setie la tabla de limpieza borre la tabla intermedia de la relacion de muchos a muchos

--- Vista/Data/Models/Personales/Bombero.cs
+++ Vista/Data/Models/Personales/Bombero.cs
@@ -65,7 +65,9 @@ namespace Vista.Data.Models.Personales
 
         public List<HorarioBombero> Horarios { get; set; } = new();
         public List<Limpieza> Limpieza { get; set; } = new();
-        
-        
+
+        public int MovilId { get; set; }
+        public Movil? MovilLimpieza { get; set; }
+
     }
 }
--- Vista/Data/Models/Salidas/Componentes/Limpieza.cs
+++ Vista/Data/Models/Salidas/Componentes/Limpieza.cs
@@ -1,4 +1,5 @@
-﻿using Vista.Data.Enums;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Vista.Data.Enums;
 using Vista.Data.Models.Personales;
 
 namespace Vista.Data.Models.Salidas.Componentes
@@ -6,10 +7,11 @@ namespace Vista.Data.Models.Salidas.Componentes
     public class Limpieza
     {
         public int LimpiezaId { get; set; }
+        public DateTime Fecha { get; set; }
         public TipoIncidente Incidente { get; set; }
         public string? Descripcion  { get; set; }
         public int? MovilId  { get; set; }
         public Movil Movil { get; set; }
-        public List<Bombero> Bomberos { get; set; }
+        public Bombero Bomberos { get; set; }
     }
 }
