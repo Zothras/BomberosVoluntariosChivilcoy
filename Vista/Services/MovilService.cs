8eb7ac03573e67395d4cae532006963f987f165a
IgnGom06 <168242982+IgnGom06@users.noreply.github.com>
2024-07-04 11:58:45 -0300

    Correción modulo Moviles
    - Se implemento MovilService
    - Se corigieron errores al agregar un movil
    - Se corrigieron errores al editar un movil
    - Se corrigieron errores al dar de baja un movil
    - Se corrigio la pagina de moviles de baja y errores respecto a ver detalles, editar y dar de alta

--- Vista/Services/MovilService.cs
+++ Vista/Services/MovilService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using DocumentFormat.OpenXml.InkML;
+using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Vista.Data;
 using Vista.Data.Enums;
@@ -11,7 +13,9 @@ namespace Vista.Services
 {
     public interface IMovilService
     {
-
+        Task<Movil> AgregarMovil(Movil movil);
+        Task<Movil> EditarMovil(Movil movil);
+        Task<Movil> CambiarEstado(int movilid, TipoEstadoMovil estado);
     }
 
     public class MovilService : IMovilService
@@ -21,6 +25,56 @@ namespace Vista.Services
         public MovilService(BomberosDbContext context)
         {
             _context = context;
+        }
+        public async Task<Movil> AgregarMovil(Movil movil)
+        {
+            Bombero? Encargado = await _context.Bomberos.SingleOrDefaultAsync(b => b.PersonaId == movil.Encargado.PersonaId);
+            movil.Encargado = Encargado;
+            _context.Moviles.Add(movil);
+            await _context.SaveChangesAsync();
+            return movil;
+        }
+        public async Task<Movil> EditarMovil(Movil movil)
+        {
+            try
+            {
+                Movil Editar = await _context.Moviles.SingleOrDefaultAsync(e => e.VehiculoId == movil.VehiculoId);
+                Bombero? Encargado = await _context.Bomberos.SingleOrDefaultAsync(b => b.PersonaId == movil.Encargado.PersonaId);
+                foreach (var i in movil.GetType().GetProperties())
+                {
+                    var propNombre = i.Name;
+                    var propValor = i.GetValue(movil);
+                    var editarProp = Editar.GetType().GetProperty(propNombre);
+                    //Validaciones.
+                    //Encargado, EncargadoId se establecen a parte para evitar errores
+                    //Seguro no esta implementado en la edición actualmente
+                    if (editarProp != null && editarProp.CanWrite && propValor != null && propNombre != "Encargado" && propNombre != "SeguroId" && propNombre != "EncargadoId")
+                    {
+                        editarProp.SetValue(Editar, propValor);
+                    }
+                }
+                Editar.Encargado = Encargado;
+                Editar.EncargadoId = Encargado.PersonaId;
+                await _context.SaveChangesAsync();
+                return movil;
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine($"Error al editar el movil {ex.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inesperado {ex.Message}");
+                return null;
+            }
+        }
+        public async Task<Movil> CambiarEstado(int movilid, TipoEstadoMovil estado)
+        {
+            Movil movil = await _context.Moviles.SingleOrDefaultAsync(e => e.VehiculoId == movilid);
+            movil.Estado = estado;
+            await _context.SaveChangesAsync();
+            return movil;
         }
     }
 }
