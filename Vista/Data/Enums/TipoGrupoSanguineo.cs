6f86316dde9cb990be7b13d2b9f7ea4035f03c25
FoxZKI <scoutfx2@gmail.com>
2024-08-09 12:25:52 -0300

    Grupo Sanguineo: Se agrego DisplayName

--- Vista/Data/Enums/TipoGrupoSanguineo.cs
+++ Vista/Data/Enums/TipoGrupoSanguineo.cs
@@ -1,14 +1,21 @@
-﻿namespace Vista.Data.Enums
-{
+﻿namespace Vista.Data.Enums;
+using System.ComponentModel.DataAnnotations;
     public enum TipoGrupoSanguineo
     {
-        APositivo,
-        ANegativo,
-        BPositivo,
-        BNegativo,
-        ABPositivo,
-        ABNegativo,
-        OPositivo,
-        ONegativo
-    }
+    [Display(Name = "A Positivo")]
+    APositivo,
+    [Display(Name = "A Negativo")]
+    ANegativo,
+    [Display(Name = "B Positivo")]
+    BPositivo,
+    [Display(Name = "B Negativo")]
+    BNegativo,
+    [Display(Name = "AB Positivo")]
+    ABPositivo,
+    [Display(Name = "AB Negativo")]
+    ABNegativo,
+    [Display(Name = "O Positivo")]
+    OPositivo,
+    [Display(Name = "O Negativo")]
+    ONegativo
 }
