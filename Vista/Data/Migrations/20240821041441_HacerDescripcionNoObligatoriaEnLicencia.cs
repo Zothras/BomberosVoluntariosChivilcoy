7222b196bf93d114bbe2cbae827c79a099fff85b
Bernard2806 <bgonzalezerramuspe@eest1chivilcoy.edu.ar>
2024-08-21 01:18:58 -0300

    El campo `Descripcion` en `Licencia` ya no es obligatorio
    Se ha actualizado la entidad 'Licencia' para que el campo 'Descripcion' pueda ser nulo. Esto permite que el campo no sea requerido en las operaciones de creación y actualización, facilitando la flexibilidad en la gestión de las licencias.

---
+++ Vista/Data/Migrations/20240821041441_HacerDescripcionNoObligatoriaEnLicencia.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class HacerDescripcionNoObligatoriaEnLicencia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Licencias",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Licencias",
+                keyColumn: "Descripcion",
+                keyValue: null,
+                column: "Descripcion",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Licencias",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Vista/Data/Models/Salidas/Componentes/Licencia.cs
+++ Vista/Data/Models/Salidas/Componentes/Licencia.cs
@@ -10,12 +10,11 @@ namespace Vista.Data.Models.Salidas.Componentes
         public int LicenciaId { get; set; }
         [Required]
         public TipoLicencia TipoLicencia { get; set; }
-        public string Descripcion { get; set; }
-        //desde = emicion de licencia
-        //Hasta = finalizacion de la licencia
-        public DateTime Desde { get; set; }
-        public DateTime Hasta { get; set; }
-    
+        public string? Descripcion { get; set; }
+
+        public DateTime Desde { get; set; } //Emision de la Licencia
+        public DateTime Hasta { get; set; } //Finalizacion de la Licencia
+
         public TipoEstadoLicencia EstadoLicencia { get; set; }
         public int PersonaId { get; set; }
         [ForeignKey("PersonaId")]
