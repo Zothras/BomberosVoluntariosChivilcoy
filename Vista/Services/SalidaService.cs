39799f418d63b03527062425c491dfc87b0f68c1
Bernard2806 <bgonzalezerramuspe@eest1chivilcoy.edu.ar>
2024-08-23 10:10:52 -0300

    Unifique la carga de todos los tipos de salida en una única función en el servicio de salida.
    La función de incendio creada por Ignacio se pudo adaptar exitosamente sin necesidad de casi ningun cambio.
    
    Co-authored-by: IgnGom06 <gomez.ignacio06@gmail.com>

--- Vista/Services/SalidaService.cs
+++ Vista/Services/SalidaService.cs
@@ -15,8 +15,7 @@ namespace Vista.Services
 {
     public interface ISalidaService
     {
-        Task<Incendio> CargarIncendio<T>(T entidad) where T : Incendio;
-        Task<MaterialPeligroso> CargarMaterialesPeligrosos<T>(T entidad) where T : MaterialPeligroso;
+        Task<Salida> CargarSalida<T>(T entidad) where T : Salida;
 
     }
 
@@ -29,7 +28,7 @@ namespace Vista.Services
             _context = context;
         }
 
-        public async Task<Incendio> CargarIncendio<T>(T salida) where T : Incendio
+        public async Task<Salida> CargarSalida<T>(T salida) where T : Salida
         {
             try
             {
@@ -105,7 +104,7 @@ namespace Vista.Services
             }
             catch (DbUpdateException ex)
             {
-                Console.WriteLine($"ERROR: Error al cargar el incendio. {ex.Message}");
+                Console.WriteLine($"ERROR: Error al cargar la salida. {ex.Message}");
                 throw;
             }
             catch (Exception ex)
@@ -114,92 +113,5 @@ namespace Vista.Services
                 throw;
             }
         }
-
-        public async Task<MaterialPeligroso> CargarMaterialesPeligrosos<T>(T salida) where T : MaterialPeligroso 
-        {
-            try
-            {
-                Bombero? bomberoEncargado = await _context.Bomberos.Where(b => b.NumeroLegajo == salida.Encargado.NumeroLegajo).SingleOrDefaultAsync();
-                Bombero? BomberoLlenoPlanilla = await _context.Bomberos.Where(b => b.NumeroLegajo == salida.QuienLleno.NumeroLegajo).SingleOrDefaultAsync();
-                Bombero? bomberoReceptor = salida.ReceptorBombero != null ? await _context.Bomberos.Where(b => b.NumeroLegajo == salida.ReceptorBombero.NumeroLegajo).SingleOrDefaultAsync() : null;
-                salida.ReceptorBombero = bomberoReceptor;
-                salida.Encargado = bomberoEncargado;
-                salida.QuienLleno = BomberoLlenoPlanilla;
-
-                //Cuerpo participante
-                List<BomberoSalida> bomberossalida = new List<BomberoSalida>();
-                foreach (BomberoSalida bom in salida.CuerpoParticipante)
-                {
-                    BomberoSalida? bomberoSalida = await _context.BomberosSalida.Where(b => b.BomberoSalidaId == bom.BomberoSalidaId).SingleOrDefaultAsync();
-                    if (bomberoSalida == null) break;
-                    BomberoSalida BomSalida = new()
-                    {
-                        Salio = bom.Salio,
-                        Bombero = bomberoSalida.Bombero,
-                    };
-                    bomberossalida.Add(BomSalida);
-                }
-                salida.CuerpoParticipante = bomberossalida;
-
-                //Moviles
-                List<MovilSalida> movilessalida = new List<MovilSalida>();
-                foreach (MovilSalida m in salida.Moviles)
-                {
-                    Bombero? bomberoChofer = await _context.Bomberos.SingleOrDefaultAsync(b => b.NumeroLegajo == m.Chofer.NumeroLegajo);
-                    Movil? Movilsalida = await _context.Moviles.SingleOrDefaultAsync(mob => mob.NumeroMovil == m.Movil.NumeroMovil);
-                    if (bomberoChofer == null || Movilsalida == null) break;
-                    Movilsalida.Kilometraje = m.KmLlegada;
-                    MovilSalida movilS = new()
-                    {
-                        CargoCombustible = m.CargoCombustible,
-                        NumeroFactura = m.NumeroFactura,
-                        FechaFactura = m.FechaFactura,
-                        TipoConbustible = m.TipoConbustible,
-                        CantidadLitros = m.CantidadLitros,
-                        QuienLleno = m.QuienLleno,
-                        TelefonoQuienLleno = m.TelefonoQuienLleno,
-                        KmLlegada = m.KmLlegada,
-                        Chofer = bomberoChofer,
-                        Movil = Movilsalida
-                    };
-                    movilessalida.Add(movilS);
-                }
-                salida.Moviles = movilessalida;
-
-                //Damnificados
-                List<Damnificado> damnificadossalida = new List<Damnificado>();
-                foreach (Damnificado d in salida.Damnificados)
-                {
-
-                    Damnificado damn = new()
-                    {
-                        Nombre = d.Nombre,
-                        Apellido = d.Apellido,
-                        Dni = d.Dni,
-                        Sexo = d.Sexo,
-                        LugarDeNacimiento = d.LugarDeNacimiento,
-                        Edad = d.Edad,
-                        Estado = d.Estado,
-                    };
-                    damnificadossalida.Add(damn);
-                }
-                salida.Damnificados = damnificadossalida;
-
-                _context.Set<T>().Add(salida);
-                await _context.SaveChangesAsync();
-                return salida;
-            }
-            catch (DbUpdateException ex)
-            {
-                Console.WriteLine($"ERROR: Error al cargar la Salida de Materiales Peligrosos. {ex.Message}");
-                throw;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"ERROR: Ocurrió un error inesperado. {ex.Message}");
-                throw;
-            }
-        }
-
     }
 }
