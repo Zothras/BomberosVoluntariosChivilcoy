1f43f0bf57ded3d12255f71bcfa45303293a1b79
Bernard2806 <107374580+Bernard2806@users.noreply.github.com>
2024-08-28 02:17:57 -0300

    Actualizada la base de datos para reflejar la relación entre BomberoSalida y MovilSalida
    - Creada una nueva migración para aplicar los cambios en el modelo `BomberoSalida`, que ahora incluye una relación con `MovilSalida`.
    - Actualizada la base de datos para incorporar esta nueva relación.

--- Vista/Data/Migrations/20240828051709_Agregar_relacion_MovilSalida_a_BomberoSalida.Designer.cs
+++ Vista/Data/Migrations/20240828051709_Agregar_relacion_MovilSalida_a_BomberoSalida.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vista.Data;
 
@@ -11,9 +12,11 @@ using Vista.Data;
 namespace Vista.Data.Migrations
 {
     [DbContext(typeof(BomberosDbContext))]
-    partial class BomberosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240828051709_Agregar_relacion_MovilSalida_a_BomberoSalida")]
+    partial class Agregar_relacion_MovilSalida_a_BomberoSalida
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Vista/Data/Migrations/20240828051709_Agregar_relacion_MovilSalida_a_BomberoSalida.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregar_relacion_MovilSalida_a_BomberoSalida : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salio",
+                table: "BomberosSalida");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BomberosSalida_MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida",
+                column: "MovilAsignadoMovilSalidaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BomberosSalida_MovilesSalida_MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida",
+                column: "MovilAsignadoMovilSalidaId",
+                principalTable: "MovilesSalida",
+                principalColumn: "MovilSalidaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BomberosSalida_MovilesSalida_MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BomberosSalida_MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida");
+
+            migrationBuilder.DropColumn(
+                name: "MovilAsignadoMovilSalidaId",
+                table: "BomberosSalida");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Salio",
+                table: "BomberosSalida",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
