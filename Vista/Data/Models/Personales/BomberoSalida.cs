d21ca8209ed1719f7c644df83758466a7104dc83
Bernard2806 <107374580+Bernard2806@users.noreply.github.com>
2024-08-30 11:36:55 -0300

    Agragadas Foreign Key para BomberoSalida
    - Añadido la migración `AddForeignKeysForBomberoSalida` para reflejar los cambios en la base de datos.
    - Se realizaron Arreglos en la Salida `IncendiosComerciales` de como se carga el Cuerpo de la Salida.

--- Vista/Data/Models/Personales/BomberoSalida.cs
+++ Vista/Data/Models/Personales/BomberoSalida.cs
@@ -1,6 +1,7 @@
 ﻿using Vista.Data.Enums;
 using Vista.Data.Models.Salidas.Planillas;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
 
 namespace Vista.Data.Models.Personales
 {
@@ -8,10 +9,22 @@ namespace Vista.Data.Models.Personales
     {
         public int BomberoSalidaId { get; set; }
 
+        // Relación con Salida
+        public int SalidaId { get; set; }
+
+        [ForeignKey(nameof(SalidaId))]
+        public Salida Salida { get; set; }
+
+        // Relación con el Movil
+        public int? MovilId { get; set; }
+
         public Movil? MovilAsignado { get; set; }
+
         public EscalafonJerarquico Grado { get; set; }
 
+        // Relación con el Bombero
         public int PersonaId { get; set; }
+
         [ForeignKey("PersonaId")]
         public Bombero Bombero { get; set; }
     }
