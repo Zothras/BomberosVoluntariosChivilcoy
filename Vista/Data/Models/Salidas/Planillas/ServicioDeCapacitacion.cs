22034af53e929eedb14bb1a4e11bc3480251e583
AleDea004 <alelucero377@gmail.com>
2024-09-23 10:38:53 -0300

    Agregue un modelo y  un ViewModel para el servicio de capacitación
    - Cree el modelo de ServicioDeCapacitacion con propiedades NivelDeCapacitacion y TipoCapacitacion y el ViewModel  con listas de días y horarios de capacitación.
    - Definición de enums para NivelDeCapacitacion y TipoCapacitacion(Ya estaba definido).

---
+++ Vista/Data/Models/Salidas/Planillas/ServicioDeCapacitacion.cs
@@ -0,0 +1,12 @@
+﻿using Vista.Data.Enums;
+
+namespace Vista.Data.Models.Salidas.Planillas
+{
+    public class ServicioDeCapacitacion : ServicioEspecial
+    {
+        public NivelDeCapacitacion NivelDeCapacitacion { get; set; }
+        public TipoCapacitacion TipoCapacitacion { get; set; }
+        public List<DateTime> DiasCapacitacion { get; set; }
+        public List<TimeSpan> HorariosCapacitacion { get; set; }
+    }
+}
---
+++ Vista/Data/ViewModels/Servicios/ServicioDeCapacitacionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Vista.Data.Enums;
+
+namespace Vista.Data.ViewModels.Servicios
+{
+    public class ServicioDeCapacitacionViewModel : SalidasViewModels 
+    {
+        public NivelDeCapacitacion NivelDeCapacitacion { get; set; }
+        public TipoCapacitacion TipoCapacitacion { get; set; }
+
+       
+        public List<DateTime> DiasCapacitacion { get; set; } = new List<DateTime>();
+        public List<TimeSpan> HorariosCapacitacion { get; set; } = new List<TimeSpan>();
+    }
+}
