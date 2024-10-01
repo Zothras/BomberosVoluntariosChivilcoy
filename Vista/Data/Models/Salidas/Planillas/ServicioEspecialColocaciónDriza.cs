567686167737cb7624fae87e7476d4952167afa7
Ezequiel M. Jaime <ezejaimee7545@gmail.com>
2024-10-01 08:20:02 -0300

    Agregue las variables faltantes en el model de colocacion de driza #140

--- Vista/Data/Models/Salidas/Planillas/ServicioEspecialColocaciónDriza.cs
+++ Vista/Data/Models/Salidas/Planillas/ServicioEspecialColocaciónDriza.cs
@@ -1,6 +1,14 @@
-﻿namespace Vista.Data.Models.Salidas.Planillas
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.Models.Salidas.Planillas
 {
     public class ServicioEspecialColocaciónDriza : ServicioEspecial
     {
+        [Required, StringLength(255)]
+        public string TipoLugar { get; set; }
+        [Required, StringLength(255)]
+        public string NombreEstablecimiento { get; set; }
+        [StringLength(255)]
+        public string? Detalles { get; set; }
     }
 }
