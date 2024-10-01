eb6d1bd74ebba90a06ede25b856158d01119d172
AleDea004 <alelucero377@gmail.com>
2024-10-01 11:15:54 -0300

    Implementacion de la salida Falsa Alarma #136

---
+++ Vista/Data/ViewModels/Servicios/ServicioEspecialFalsaAlarmaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.ViewModels.Servicios
+{
+    public class ServicioEspecialFalsaAlarmaViewModel : SalidasViewModels
+    {
+        [Required, StringLength(255)]
+        public string Detalles { get; set; }
+    }
+}
