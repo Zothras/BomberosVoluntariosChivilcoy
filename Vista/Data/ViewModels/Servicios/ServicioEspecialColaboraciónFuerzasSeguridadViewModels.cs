be609a0244485e18a90b3f54309ef5b19e262df2
Ezequiel M. Jaime <ezejaimee7545@gmail.com>
2024-10-02 09:46:38 -0300

    Se debe crear la salida de Colaboración con Fuerzas de Seguridad #141 Cree el modelo y la vista de la salida Colaboración con Fuerzas de Seguridad

---
+++ Vista/Data/ViewModels/Servicios/ServicioEspecialColaboraciónFuerzasSeguridadViewModels.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.ViewModels.Servicios
+{
+    public class ServicioEspecialColaboraciónFuerzasSeguridadViewModels : SalidasViewModels
+    {
+        [StringLength(255)]
+        public string? DetallesColaboFuerzasSeguridad { get; set; }
+    }
+}
