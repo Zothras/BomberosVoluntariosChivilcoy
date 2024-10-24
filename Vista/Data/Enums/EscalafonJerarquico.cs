6af27ae8fdfc56bfb24e0d0a668372457abef1f9
Ezequiel M. Jaime <ezejaimee7545@gmail.com>
2024-10-24 11:43:47 -0300

    Solución a la Duplicación de Bomberos en la Lista de Ascensos. #154
    Modifique el método CargarAscenso() para limpiar la lista de ascensos antes de cargar nuevos datos. Esto asegura que cada vez que se carguen los ascensos, la lista comience vacía, evitando duplicaciones.
    
    Ademas agregue un nuevo Escalafon Jerarquico para aquellas bomberos que recien ingresan al cuartel.

--- Vista/Data/Enums/EscalafonJerarquico.cs
+++ Vista/Data/Enums/EscalafonJerarquico.cs
@@ -1,31 +1,33 @@
 ﻿namespace Vista.Data.Enums;
 using System.ComponentModel.DataAnnotations;
 
-    public enum EscalafonJerarquico
+public enum EscalafonJerarquico
 {
-        [Display(Name = "Jefe")]
-        Jefe,
-        [Display(Name = "2° Jefe")]
-        Segundojefe,
-        [Display(Name = "1° Oficial")]
-        PrimerOficial,
-        [Display(Name = "2° Ofical")]
-        SegundoOficial,
-        [Display(Name = "3° Oficial")]
-        TercerOficial,
-        [Display(Name = "Auxiliar Mayor")]
-        AuxiliarMayor,
-        [Display(Name = "1° Auxiliar")]
-        PrimerAuxiliar,
-        [Display(Name = "2° Auxiliar")]
-        SegundoAuxiliar,
-        [Display(Name = "1° Ayudante")]
-        PrimerAyudante,
-        [Display(Name = "2° Ayudante")]
-        SegundoAyudante,
-        [Display(Name = "Bombero")]
-        Bombero,
-        [Display(Name = "Aspirante")]
-        Aspirante,
+    [Display(Name = "Jefe")]
+    Jefe,
+    [Display(Name = "2° Jefe")]
+    Segundojefe,
+    [Display(Name = "1° Oficial")]
+    PrimerOficial,
+    [Display(Name = "2° Ofical")]
+    SegundoOficial,
+    [Display(Name = "3° Oficial")]
+    TercerOficial,
+    [Display(Name = "Auxiliar Mayor")]
+    AuxiliarMayor,
+    [Display(Name = "1° Auxiliar")]
+    PrimerAuxiliar,
+    [Display(Name = "2° Auxiliar")]
+    SegundoAuxiliar,
+    [Display(Name = "1° Ayudante")]
+    PrimerAyudante,
+    [Display(Name = "2° Ayudante")]
+    SegundoAyudante,
+    [Display(Name = "Bombero")]
+    Bombero,
+    [Display(Name = "Aspirante")]
+    Aspirante,
+    [Display(Name = "")]
+    SinRango,
 }
 
