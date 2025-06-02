c59f5d971ee878c2a1aceef5c647361369ec2450
Bernardo <berramuspegonzalez@gmail.com>
2025-06-02 07:05:20 -0300

    feat(salidas): agregar enums categorizados para tipos de emergencias y servicios (#199)
    Se añadieron los enums separados por categoría: IncendioTipo, RescateTipo, ServicioEspecialTipo y CategoriaEmergencia.
    Esta separación permite organizar mejor los tipos de salidas de emergencia y facilita la implementación de un modal dinámico para crear nuevas salidas en la aplicación. Además, mejora la legibilidad del código y evita la duplicación de valores.

---
+++ Vista/Data/Enums/Salidas/RescateTipo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.Enums.Salidas
+{
+    /// <summary>
+    /// Tipos de Rescate
+    /// </summary>
+    public enum RescateTipo
+    {
+        /// <summary>
+        /// Rescate de Persona
+        /// </summary>
+        [Display(Name = "Persona")]
+        Persona,
+
+        /// <summary>
+        /// Rescate de Animal
+        /// </summary>
+        [Display(Name = "Animal")]
+        Animal
+    }
+}
